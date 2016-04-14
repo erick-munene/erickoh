1d4d03ffd9065fd337ed9c991e75cc785085649a
Erick-Munene <munenewahome@gmail.com>
2016-04-14 14:45:29 +0300

    added a loader

--- js/app.js
+++ js/app.js
@@ -1,1 +1,10 @@
 $(document).foundation()
+
+$(document).ready(function() {
+ 
+    setTimeout(function(){
+        $('body').addClass('loaded');
+        $('h1').css('color','#222222');
+    }, 3000);
+ 
+});
