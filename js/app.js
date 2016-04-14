fcc24107b6ced7dbb81f40de519d67d29e7507d6
Erick-Munene <munenewahome@gmail.com>
2016-04-14 22:15:00 +0300

    added new responsive nav

--- js/app.js
+++ js/app.js
@@ -8,3 +8,21 @@ $(document).ready(function() {
     }, 3000);
  
 });
+
+
+$(document).ready(function(){
+
+	(function($) {
+
+		$('#header__icon').click(function(e){
+			e.preventDefault();
+			$('body').toggleClass('with--sidebar');
+		});
+    
+    $('#site-cache').click(function(e){
+      $('body').removeClass('with--sidebar');
+    });
+
+	})(jQuery);
+
+});
