9c8d5ff419a33f8bafc59e80ec40f5b705c1d567
Erick-Munene <munenewahome@gmail.com>
2016-04-18 16:01:27 +0300

    modified the body and navigation

--- js/app.js
+++ js/app.js
@@ -1,82 +1,38 @@
 $(document).foundation()
 
-$(document).ready(function() {
- 
-    setTimeout(function(){
-        $('body').addClass('loaded');
-        $('h1').css('color','#222222');
-    }, 3000);
- 
+// Sticky Header
+$(window).scroll(function() {
+
+    if ($(window).scrollTop() > 100) {
+        $('.main_h').addClass('sticky');
+    } else {
+        $('.main_h').removeClass('sticky');
+    }
 });
 
-
-$(document).ready(function(){
-
-	(function($) {
-
-		$('#header__icon').click(function(e){
-			e.preventDefault();
-			$('body').toggleClass('with--sidebar');
-		});
-    
-    $('#site-cache').click(function(e){
-      $('body').removeClass('with--sidebar');
-    });
-
-	})(jQuery);
-
+// Mobile Navigation
+$('.mobile-toggle').click(function() {
+    if ($('.main_h').hasClass('open-nav')) {
+        $('.main_h').removeClass('open-nav');
+    } else {
+        $('.main_h').addClass('open-nav');
+    }
 });
 
-/*............................................*/
-
-$(document).ready(function() {
-
-  var curPage = 1;
-  var numOfPages = $(".skw-page").length;
-  var animTime = 1000;
-  var scrolling = false;
-  var pgPrefix = ".skw-page-";
-
-  function pagination() {
-    scrolling = true;
-
-    $(pgPrefix + curPage).removeClass("inactive").addClass("active");
-    $(pgPrefix + (curPage - 1)).addClass("inactive");
-    $(pgPrefix + (curPage + 1)).removeClass("active");
-
-    setTimeout(function() {
-      scrolling = false;
-    }, animTime);
-  };
-
-  function navigateUp() {
-    if (curPage === 1) return;
-    curPage--;
-    pagination();
-  };
-
-  function navigateDown() {
-    if (curPage === numOfPages) return;
-    curPage++;
-    pagination();
-  };
-
-  $(document).on("mousewheel DOMMouseScroll", function(e) {
-    if (scrolling) return;
-    if (e.originalEvent.wheelDelta > 0 || e.originalEvent.detail < 0) {
-      navigateUp();
-    } else { 
-      navigateDown();
+$('.main_h li a').click(function() {
+    if ($('.main_h').hasClass('open-nav')) {
+        $('.navigation').removeClass('open-nav');
+        $('.main_h').removeClass('open-nav');
     }
-  });
+});
 
-  $(document).on("keydown", function(e) {
-    if (scrolling) return;
-    if (e.which === 38) {
-      navigateUp();
-    } else if (e.which === 40) {
-      navigateDown();
-    }
-  });
-
+// navigation scroll lijepo radi materem
+$('nav a').click(function(event) {
+    var id = $(this).attr("href");
+    var offset = 70;
+    var target = $(id).offset().top - offset;
+    $('html, body').animate({
+        scrollTop: target
+    }, 500);
+    event.preventDefault();
 });
